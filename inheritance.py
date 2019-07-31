131754528157180fa15df5fad8ffdaa6e82287d1
marcomereu2015 <53127114+marcomereu2015@users.noreply.github.com>
2019-07-31 09:40:38 -0500

    Add files via upload

---
+++ inheritance.py
@@ -0,0 +1,60 @@
+#Day 12 : Inherintance
+
+
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print "Name:", self.lastName + ",", self.firstName
+		print "ID:", self.idNumber
+
+class Student(Person):
+
+    #   Class Constructor
+    #   
+    #   Parameters:
+    #   firstName - A string denoting the Person's first name.
+    #   lastName - A string denoting the Person's last name.
+    #   id - An integer denoting the Person's ID number.
+    #   scores - An array of integers denoting the Person's test scores.
+    #
+    # Write your constructor here
+    def __init__(self,firstName,lastName,idNum,scores):
+        self.firstName=firstName
+        self.lastName=lastName
+        self.idNumber=idNum
+        self.scores=scores
+
+    def calculate(self):
+        avg = sum(self.scores) / len(self.scores)
+    #   Function Name: calculate
+    #   Return: A character denoting the grade.
+    #
+    # Write your function here
+        a = ''
+        if (90 <= avg <= 100):
+            a = 'O'
+        elif (80 <= avg < 90):
+            a = 'E'
+        elif (70 <= avg < 80):
+            a = 'A'   
+        elif (55 <= avg < 70):
+            a = 'P'
+        elif (40 <= avg < 55):
+            a = 'D'
+        elif (avg < 40):
+            a = 'T'
+        return a
+
+        
+line = raw_input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(raw_input()) # not needed for Python
+scores = map(int, raw_input().split())
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print "Grade:", s.calculate()
